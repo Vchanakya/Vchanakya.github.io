27fa085e66b6c47d770010277f6f4ee4d3c9e722
Vchanakya <vignan.ifocus@gmail.com>
2020-05-29 15:16:57 +0530

    Landing Page Vivek Suggestions 1

--- js/loader.js
+++ js/loader.js
@@ -1,109 +1,53 @@
 function animations() {
-    let loopStatus = true;
+    let loopCount = 0;
     let changeStatus = true;
-    let textFills = [
-        "Things",
-        "Projects",
-        "IoT",
-        "Automation",
-        "Edge",
-        "Applications",
-        "Control Panels",
-        "Internet of Things",
-    ];
+    let word = "";
+    let wordChange = "";
+    let textFills = ["Ideas", "Products", "Learning", "Services", "Solutions", "Technology", "Insights"];
 
     anime({
-        targets: "#logo1 svg path",
-        easing: "easeInOutSine",
-        fill: "#FFFFFF",
-        duration: 1400,
-        delay: (el, i) => {
-            return i * 100;
-        },
-        direction: "alternate",
-        loop: true,
-        loopBegin: () => {
-            if (loopStatus) {
-                textAnimation.play;
-                loopStatus = false;
-            } else {
-                textAnimation.pause;
-                loopStatus = true;
-            }
-        },
-    });
-
-    anime({
-        targets: "#logo2 svg path",
-        easing: "easeInOutSine",
-        fill: "#FFFFFF",
-        duration: 1400,
-        delay: (el, i) => {
-            return i * 100;
-        },
-        direction: "alternate",
-        loop: true,
-        loopBegin: () => {
-            if (loopStatus) {
-                textAnimation.play;
-                loopStatus = false;
-            } else {
-                textAnimation.pause;
-                loopStatus = true;
-            }
-        },
-    });
-
-    let textAnimation = anime({
         targets: ".tagline",
         easing: "linear",
         color: ["#000000", "#1f1868", "#173576", "#0053bc", "#22b128", "#d0bd00", "#ec6a08", "#e40905"],
         duration: 7000,
         loop: true,
         direction: "alternate",
-        changeBegin: () => {
-            if (changeStatus) {
-                console.log("Full");
-                anime({
-                    targets: ".textRotate",
-                    opacity: [0, 1],
-                    easing: "linear",
-                    duration: 3500,
-                    delay: (el, i) => {
-                        el.classList.remove("opacity-zero");
-                        el.classList.add("opacity-full");
-                        return i * 100;
-                    },
-                });
-                changeStatus = false;
+    });
+
+    anime({
+        targets: "#blink",
+        easing: "linear",
+        duration: 300,
+        loop: true,
+        direction: "alternate",
+        opacity: [1, 0],
+        loopBegin: () => {
+            if (word === "") {
+                wordChange = textFills[Math.floor(Math.random() * textFills.length)];   
+                word = wordChange[0];
+                loopCount = 0;
             } else {
-                console.log("Zero");
-                anime({
-                    targets: ".textRotate",
-                    opacity: [1, 0],
-                    easing: "linear",
-                    duration: 3500,
-                    endDelay: 100,
-                    delay: (el, i) => {
-                        return (document.querySelectorAll(".textRotate").length - i) * 100;
-                    },
-                    complete: () => {
-                        let tempText = textFills[Math.floor(Math.random() * textFills.length)];
-                        document.querySelector("#textFills").innerHTML = tempHtml(tempText);
-                    },
-                });
-                changeStatus = true;
+                if (changeStatus) {
+                    loopCount++;
+                    if (loopCount > wordChange.length) changeStatus = false;
+                    else {
+                        word = wordChange.slice(0, loopCount);
+                        document.getElementById("words").innerText = word;
+                    }
+                } else {
+                    loopCount--;
+                    if (loopCount === 0) {
+                        changeStatus = true;
+                        word = "";
+                        document.getElementById("words").innerText = word;
+                    } else {
+                        word = wordChange.slice(0, loopCount);
+                        document.getElementById("words").innerText = word;
+                    }
+                }
             }
         },
     });
-
-    function tempHtml(tempText) {
-        let tempStr = "";
-        for (let i = 0; i < tempText.length; i++) {
-            tempStr += `<div class="textRotate d-inline opacity-zero">${tempText[i].toUpperCase()}</div>`;
-        }
-        return tempStr;
-    }
 }
 
 animations();
