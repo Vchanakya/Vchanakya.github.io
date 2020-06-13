d74f40bf4782a805bfaeb8238a2e57b102046284
Vchanakya <vignan.ifocus@gmail.com>
2020-06-13 11:18:59 +0530

    Designed Stage 1

--- js/index.js
+++ js/index.js
@@ -1,28 +1,34 @@
-document.getElementById("menu-services").onclick = () => {
-    document.querySelectorAll(".sectionpage").forEach((el) => {
-        el.classList.remove("d-block");
-        el.classList.add("d-none");
-    });
-    document.getElementById("services").classList.remove("d-none");
-    document.getElementById("services").classList.add("d-block");
-};
-
-document.querySelectorAll(".hovercard").forEach((el) => {
-    el.addEventListener("mouseover", () => {
-        document.querySelectorAll(".hovercard").forEach((el) => {
-            el.classList.remove("shadow-lg");
-            el.classList.remove("opacity-full");
-            el.classList.add("opacity-half");
+function indexMain() {
+    const pageHeight = $("#mainPage").height() - $("header").outerHeight(true) - $("footer").outerHeight(true);
+    window.onload = () => {
+        $("main").css("min-height", pageHeight);
+        $(".singlepage").css("min-height", pageHeight);
+        $(".mediumpage").css("min-height", 0.8 * pageHeight);
+        $("#landingImage").css("max-height", pageHeight / 2);
+        $("#architectureImage").css("max-height", pageHeight);
+    };
+    document.querySelectorAll("header a").forEach((el) => {
+        el.addEventListener("click", (ev) => {
+            document.querySelectorAll("section").forEach((sectionEl) => {
+                if (sectionEl.getAttribute("id") !== el.getAttribute("data-href").split("#")[1])
+                    sectionEl.classList.add("d-none");
+                else sectionEl.classList.remove("d-none");
+            });
+            document.querySelectorAll("#navbarMenu a").forEach((aEl) => {
+                if (aEl === el) el.classList.add("text-underline");
+                else aEl.classList.remove("text-underline");
+            });
+            document.querySelector("#navbarMenu").classList.remove("show");
         });
-        el.classList.add("shadow-lg");
-        el.classList.add("opacity-full");
     });
-});
+    $("#aboutMenu").click(() => {
+        window.scrollTo(0, 0.8 * pageHeight);
+    });
 
-document.querySelectorAll(".hovercard").forEach((el) => {
-    el.addEventListener("mouseout", () => {
-        el.classList.remove("shadow-lg");
-        el.classList.remove("opacity-full");
-        el.classList.add("opacity-half");
+    $("a[data-href='#aboutus']").click(() => {
+        console.log($("#aboutItem").height(), $("#aboutMenu").height());
+        $("#pills-tabContent").css("min-height", $("#aboutItem").height() - $("#aboutMenu").height());
     });
-});
+}
+
+indexMain();
